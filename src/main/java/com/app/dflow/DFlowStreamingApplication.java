311552a67c09a2fca9dc4b21157f8f0df4f92c13
alex georgescu <alexgeorgescu98@gmail.com>
2020-12-20 01:11:11 +0200

    dFlow-0001: rename application

--- src/main/java/com/app/dflow/DFlowStreamingApplication.java
+++ src/main/java/com/app/dflow/DFlowStreamingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DFlowApplication {
+public class DFlowStreamingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DFlowApplication.class, args);
+		SpringApplication.run(DFlowStreamingApplication.class, args);
 	}
 
 }
